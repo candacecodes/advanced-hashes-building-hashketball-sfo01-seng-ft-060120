72b0aad442122c8404a0bdbf74aeff44718ffd0c
eager-class-5814 <candacehchoe@gmail.com>
2020-05-21 21:29:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,9 +1,9 @@
 # Write your code here!
 def game_hash
   :home
-    :team_name
-    :colors 
-    :players
+    :team_name => string 
+    :colors => [] 
+    :players => [{:player_name, :number, :shoe, :points, :rebounds, :assists, :steals, :blocks, :slam_dunks}]
   :away 
     :team_name
     :colors 
