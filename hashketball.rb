a280eebc6530c9111e0ff2facc7ce4ff380e58a4
eager-class-5814 <candacehchoe@gmail.com>
2020-05-21 21:41:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -52,8 +52,7 @@ def game_hash
   away: {
       team_name: "Charlotte Hornets"
       colors: ["Turquoise", "Purple"]
-      players: [
-        {
+      players: [{
       player_name: "Jeff Adrien", 
       number: 4, 
       shoe: 18, 
