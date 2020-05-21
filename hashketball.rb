647481883d2dc717cddf000b0de5f3f09e866387
eager-class-5814 <candacehchoe@gmail.com>
2020-05-21 22:01:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -26,6 +26,16 @@ def game_hash
         blocks: 12, 
         slam_dunks: 7
       }, {
+        player_name: "Brook Lopez", 
+        number: 11, 
+        shoe: 17, 
+        points: 17, 
+        rebounds: 19, 
+        assists: 10, 
+        steals: 3, 
+        blocks: 12, 
+        slam_dunks: 7
+      }, {
         player_name: "Mason Plumlee", 
         number: 1, 
         shoe: 19, 
