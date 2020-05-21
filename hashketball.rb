5b1bb7ffb9579f8d284527bc3f22407a275d1aac
eager-class-5814 <candacehchoe@gmail.com>
2020-05-21 21:59:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -51,7 +51,7 @@ def game_hash
         
         
     away: {
-        team_name: "Charlotte Hornets"
+        team_name: ["Charlotte Hornets"]
         colors: ["Turquoise", "Purple"]
         players: [
           {
